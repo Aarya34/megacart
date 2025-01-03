658c58371810984aaa16ae2c89f1455308dff742
Aarya34 <142531338+Aarya34@users.noreply.github.com>
2025-01-03 08:34:08 +0530

    Add Redux integration with store configuration and constants; update package dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import HomeScreen from './screens/HomeScreen'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import App from './App.jsx'
+import {Provider} from 'react-redux'
+import store from './store'
 import ProductScreen from './screens/ProductScreen.jsx'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -19,6 +21,8 @@ const routes = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <RouterProvider router={routes} />
+    </Provider>
   </StrictMode>,
 )
