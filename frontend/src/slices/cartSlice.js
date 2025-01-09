4938d590383a349dcefbe382b843bd77f6003261
Aarya34 <142531338+Aarya34@users.noreply.github.com>
2025-01-09 13:18:53 +0530

    Bug fix

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -17,11 +17,11 @@ const cartSlice = createSlice({
             const existItem = state.cartItems.find(x => x.product._id === item.product._id);
             if(existItem){
                 existItem.qty += item.qty;
-                // state.cartItems = state.cartItems.map(x => x.product === existItem.product ? item : x);
+                
             }else{
                 state.cartItems = [...state.cartItems, item];
             }
-            // state.qty+= item.qty;
+
             state.itemsPrice = state.cartItems.reduce((acc, item) => acc + item.product.price * item.qty, 0);
             state.shippingPrice = state.itemsPrice > 100 ? 0 : 10;
             state.taxPrice = Number((0.15 * state.itemsPrice).toFixed(2));
@@ -34,13 +34,9 @@ const cartSlice = createSlice({
             existItem.qty = item.qty;
         },
         removeItem: (state, action) => {
-            console.log(action.payload)
             state.cartItems = state.cartItems.filter((item) => {
-                console.log('Comparing:', item.product._id, action.payload);
-                return item.product._id !== action.payload;
+                return item.product._id !== action.payload.id;
             });
-            console.log(state.cartItems)
-            
             localStorage.setItem('cart', JSON.stringify(state));
         }
     },
