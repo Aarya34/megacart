1b1a52cd8f7f5fee7f088dad505ea413ab3569e3
Aarya34 <142531338+Aarya34@users.noreply.github.com>
2025-01-16 04:07:56 +0530

    Integrate PayPal payment processing with API endpoint and update order status handling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {PayPalScriptProvider} from '@paypal/react-paypal-js'
 import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider,Router,Routes } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import './assets/styles/bootstrap.custom.css'
@@ -15,6 +16,7 @@ import ShippingScreen from './screens/ShippingScreen.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import PaymentScreen from './screens/PaymentScreen.jsx'
 import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx'
+import OrderScreen from './screens/OrderScreen.jsx'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 const routes = createBrowserRouter(
@@ -31,6 +33,7 @@ const routes = createBrowserRouter(
         <Route path="shipping" element={<ShippingScreen/>}/>
         <Route path="payment" element={<PaymentScreen/>}/>
         <Route path="placeorder" element={<PlaceOrderScreen/>}/>
+        <Route path="order/:id" element={<OrderScreen/>}/>
       </Route>
     </Route>
     // </Routes>
@@ -40,7 +43,9 @@ const routes = createBrowserRouter(
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <RouterProvider router={routes} />
+      <PayPalScriptProvider deferLoading={true}>
+      <RouterProvider router={routes} />
+      </PayPalScriptProvider>
     </Provider>
   </StrictMode>,
 )
--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,4 +1,4 @@
-import {ORDERS_URL} from '../constants';
+import {ORDERS_URL,PAYPAL_URL} from '../constants';
 import { apiSlice} from './apiSlice';
 
 export const ordersApi = apiSlice.injectEndpoints({
@@ -11,8 +11,27 @@ export const ordersApi = apiSlice.injectEndpoints({
                 body: {...order},
             }),
         }),
+        getOrderDetails: builder.query({
+            query: (id) => ({
+                url: `${ORDERS_URL}/${id}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        payOrder: builder.mutation({
+            query: (orderId,details ) => ({
+                url: `${ORDERS_URL}/${orderId}/pay`,
+                method: 'PUT',
+                body: {...details},
+            }),
+        }),
+        getPayPalClientId: builder.query({
+            query: () => ({
+                url: PAYPAL_URL,
+            }),
+            keepUnusedDataFor: 5,
+        }),
     }),
 }); 
 
-export const { useCreateOrderMutation} = ordersApi;
+export const { useCreateOrderMutation, useGetOrderDetailsQuery,useGetPayPalClientIdQuery,usePayOrderMutation} = ordersApi;
      
