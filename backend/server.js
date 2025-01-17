dc8a82b3efbc4f4a30ca1074f634aa458cb7ba8c
Aarya34 <142531338+Aarya34@users.noreply.github.com>
2025-01-17 18:52:45 +0530

    Bugg

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,10 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: 'http://localhost:5173', // Adjust to your frontend URL
-  credentials: true,
-}))
+// app.use(cors({
+//   origin: 'http://localhost:5173', // Adjust to your frontend URL
+//   credentials: true,
+// }))
 
 //Body parser
 app.use(express.json());
