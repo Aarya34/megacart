2ad51b60e6d3c2b283a1752c48f7010fa9633d26
Aarya34 <142531338+Aarya34@users.noreply.github.com>
2025-01-17 12:39:23 +0530

    minor1

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,53 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
-import orderRoutes from './routes/orderRoutes.js';
-import productRoutes from './routes/productRoutes.js'; 
-import uploadRoutes from './routes/uploadRoutes.js';
-import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+dotenv.config();
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import cors from 'cors';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-dotenv.config()
-
-connectDB(); 
-
-const app = express();
 const port = process.env.PORT || 5000;
 
+connectDB();
 
-//Body parser
+const app = express();
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
-
 
 app.use('/api/products', productRoutes);
-app.use('/api/users',userRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/upload',uploadRoutes);
-app.get('/api/config/paypal', (req, res) => res.send({clientId:process.env.PAYPAL_CLIENT_ID}));
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-if(process.env.NODE_ENV === 'production'){
+app.use('/api/upload', uploadRoutes);
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*',(req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
-}
-else{
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
-    res.send('Server is ready');
+    res.send('API is running....');
   });
 }
+
 app.use(notFound);
 app.use(errorHandler);
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
